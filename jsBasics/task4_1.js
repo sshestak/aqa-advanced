e6f45db3fd4cd6139bd44d908a775a1fe1609c7e
sshestak <sveta6306@gmail.com>
2026-07-23 12:59:25 +0300

    JS variables and operators exercises

---
+++ jsBasics/task4_1.js
@@ -0,0 +1,9 @@
+// Завдання 4: Обчислення площі та об'єму
+//Завдання 4.1
+
+//радіус кола
+let radius = 12;
+
+//Площа кола, формула: π * radius^2 
+let circleArea = Math.PI * radius ** 2;
+console.log(circleArea.toFixed(2));
